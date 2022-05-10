9888b22177a77de554a9dcdb986135864f701134
Afina R. Vinci <afinarv@gmail.com>
2022-05-10 18:28:33 +0700

    Initial Commit

---
+++ Custom Cell with XIB/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Custom Cell with XIB
+//
+//  Created by Afina R. Vinci on 10/05/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
